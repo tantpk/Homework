1e43a4b3f9fdc34b71f91897bb6fd58393c25e5c
tantpk <peter.tang@work.twghintranet.org>
2024-02-09 16:36:07 +0800

    update css and nuget package

--- Homework/App_Start/BundleConfig.cs
+++ Homework/App_Start/BundleConfig.cs
@@ -23,16 +23,22 @@ namespace Homework
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/bootstrap.bundle.min.js"
+                      ));
+            bundles.Add(new Bundle("~/bundles/bootstrap-select").Include(
+                      "~/Scripts/bootstrap-select.min.js",
+                      "~/Scripts/i18n/defaults-en_US.min.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/bootstrap-select.min.css",
                       "~/Content/DataTables/css/dataTables.jqueryui.min.css",
                       "~/Content/DataTables/css/dataTables.bootstrap.min.css",
-                      "~/Content/DataTables/css/dataTables.bootstrap4.css",
-                      "~/Content/DataTables/css/responsive.dataTables.min.css"
+                      "~/Content/DataTables/css/dataTables.bootstrap4.min.css",
+                      "~/Content/DataTables/css/responsive.dataTables.min.css"                      
                       ));
         }
     }
